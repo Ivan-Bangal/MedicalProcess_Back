c32008132a68dccaa8b84b04af808a0ae29a7b00
Ivandro Bangal <ivandrocabrabangal@gmail.com>
2022-09-18 22:34:30 +0200

    Adding CrossOrigin

--- src/main/java/com/example/medicalprocess/medicalprocess/controller/ConsultController.java
+++ src/main/java/com/example/medicalprocess/medicalprocess/controller/ConsultController.java
@@ -7,6 +7,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,7 +21,8 @@ import com.example.medicalprocess.medicalprocess.model.Consult;
 import com.example.medicalprocess.medicalprocess.services.ConsultService;
 
 @RestController
-@RequestMapping("/resourceRootPath")
+@RequestMapping("/Consult")
+@CrossOrigin("*")
 public class ConsultController {
 
     @Autowired
